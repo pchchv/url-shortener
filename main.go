619038c8573347fc45a23120da85ec1375fe8ec0
pchchv <ipchchv@gmail.com>
2022-07-03 13:23:09 +0300

    Implement random URL generation

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	letters       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // Number of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func getURL() string {
+	n := src.Int63()
+	for n > 8 {
+		n /= 3
+	}
+	u := generateURL(n)
+	return u
+}
+
+func generateURL(n int64) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			b[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func main() {
+	getURL()
+}
