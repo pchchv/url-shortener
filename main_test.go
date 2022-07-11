2cdbc189c9f27fe71f0eef0bcc0bf6a259979a9e
pchchv <ipchchv@gmail.com>
2022-07-11 14:37:54 +0300

    Add generate method tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -45,6 +46,44 @@ func TestLoadPing(t *testing.T) {
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL:    "http://localhost:8080/ping",
+	})
+	attacker := vegeta.NewAttacker()
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
+
+func TestPost(t *testing.T) {
+	params := []string{
+		"?url=google.com",
+		"?url=github.com&input=git",
+		"?url=github.com",
+		"?url=google.com"}
+	for _, v := range params {
+		res, err := http.Get(fmt.Sprintf("http://127.0.0.1:8080/generate" + v))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("status not OK")
+		}
+		_, err = ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		res.Body.Close()
+	}
+}
+
+func TestLoadPost(t *testing.T) {
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "POST",
+		URL:    "http://localhost:8080/generate?url=github.com/pchchv/url-shortener",
 	})
 	attacker := vegeta.NewAttacker()
 	var metrics vegeta.Metrics
